7bda8f7d374561a3fbaac5d1fb2f3253c42c4907
github1tg <88201236+github1tg@users.noreply.github.com>
2021-12-09 00:55:35 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -9,20 +9,19 @@ async def start(c, m):
 
     await m.reply_text(
         text=f"Hi 👋 there {m.from_user.mention}.\n\nI'm Screenshot Generator Bot. I can provide screenshots from "
-        "your video files without downloading the entire file (almost instantly). For more details check /help.\n\n🚨 Porn Contents will be gives you PERMANENT BAN 🚨",)
+        "your video files without downloading the entire file (almost instantly). For more details check /help.\n\n🚨 Porn Contents "
+        "will be gives you PERMANENT BAN 🚨\n\n🍃 Bᴏᴛ Made Bʏ :@MHND_KDR",
         quote=True,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(
-                        "🍬 Project channel", url="https://telegram.me/MHND_BOT_UPDATE_CHANNEL"
-                    ),
-                    InlineKeyboardButton("🍭 Support group", url="https://telegram.me/MHND_BOT_UPDATE_GROUP"),
+                    InlineKeyboardButton('🍬 Project channel', url='https://telegram.me/MHND_BOT_UPDATE_CHANNEL'),
+                    InlineKeyboardButton('🍭 Support group', url='https://telegram.me/MHND_BOT_UPDATE_GROUP')
                 ],
-                [InlineKeyboardButton(
-                     "🌹 Source code 🌹", url="https://github.com/github2tg/FILE_ANIMATING"
-                     ).
-                     InlineKeyboardButton("👩‍💻 Master", url="https://telegram.me/MHND_KDR")],
+                [
+                    InlineKeyboardButton('🌹 Source code 🌹', url='https://github.com/github2tg/FILE_ANIMATING'),
+                    InlineKeyboardButton('👩‍💻 Master', url='https://telegram.me/MHND_KDR')
+                ]
             ]
-        ),
+        )
     )
