b6ce415359ecf2b35fd627a2113f3e45345e32da
github1tg <88201236+github1tg@users.noreply.github.com>
2021-12-09 00:29:59 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -6,7 +6,14 @@ from ..screenshotbot import ScreenShotBot
 
 @ScreenShotBot.on_message(Filters.private & Filters.command("start"))
 async def start(c, m):
-
+    
+    if not await c.db.is_user_exist(m.chat.id):
+        await c.db.add_user(m.chat.id)
+        await c.send_message(
+            Config.LOG_CHANNEL,
+            f"New User [{m.from_user.first_name}](tg://user?id={m.chat.id}) started."
+        )
+    
     await m.reply_text(
         text=f"Hi 👋 there {m.from_user.mention}.\n\nI'm Screenshot Generator Bot. I can provide screenshots from "
         "your video files without downloading the entire file (almost instantly). For more details check /help.\n\n🚨 Porn Contents "
