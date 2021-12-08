00e4c8a53f5e6b4872b076fdc902752783472e60
github1tg <88201236+github1tg@users.noreply.github.com>
2021-12-09 00:16:53 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -10,7 +10,7 @@ async def start(c, m):
     await m.reply_text(
         text=f"Hi 👋 there {m.from_user.mention}.\n\nI'm Screenshot Generator Bot. I can provide screenshots from "
         "your video files without downloading the entire file (almost instantly). For more details check /help.\n\n🚨 Porn Contents "
-        "will be gives you PERMANENT BAN 🚨 /n/n🍃 Bᴏᴛ Made Bʏ :@MHND_KDR",
+        "will be gives you PERMANENT BAN 🚨./n/n🍃 Bᴏᴛ Made Bʏ :@MHND_KDR",
         quote=True,
         reply_markup=InlineKeyboardMarkup(
             [
