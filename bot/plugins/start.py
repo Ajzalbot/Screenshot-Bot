724a1d13e80fd6625d00f7453605bfd18fc54cc1
github1tg <88201236+github1tg@users.noreply.github.com>
2021-12-09 06:59:49 +0530

    Update start.py

--- bot/plugins/start.py
+++ bot/plugins/start.py
@@ -8,8 +8,8 @@ from ..screenshotbot import ScreenShotBot
 async def start(c, m):
 
     await m.reply_text(
-        text=f"Hi 👋 there {m.from_user.mention}.\n\nI'm Screenshot Generator Bot. I can provide screenshots from "
-        "your video files without downloading the entire file (almost instantly). For more details check /help.\n\n🚨 Porn Contents "
+        text=f"Hi 👋 there {m.from_user.mention}.\n\nI'm Screenshot Generator Bot.\n\nSend the file you "
+        "want to👉 [this bot](https://telegram.me/BEST_FILE_STREAM_BOT).\nThen paste the👉 [link here](https://telegram.me/SCREEN_SHOT_ROBOT)\n\nFor more details check /help.\n\n🚨 Porn Contents "
         "will be gives you PERMANENT BAN 🚨\n\n🍃 Bᴏᴛ Made Bʏ :@MHND_KDR",
         quote=True,
         reply_markup=InlineKeyboardMarkup(
