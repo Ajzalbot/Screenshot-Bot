a6bae1315ee56c4ab5fe9ad44b8a48c933a1ee60
github1tg <88201236+github1tg@users.noreply.github.com>
2021-12-10 14:37:59 +0530

    Update utils.py

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -291,7 +291,7 @@ class Utilities:
         i_keyboard = []
         for i in range(1, 20):
             i_keyboard.append(InlineKeyboardButton(f"{i}", f"scht+{i}"))
-            if (i > 1) and (i % 5) == 1:
+            if (i > 1) and (i % 4, 4) == 1:
                 btns.append(i_keyboard)
                 i_keyboard = []
             if i == 20:
