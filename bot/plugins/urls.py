f744d921f148a3b0f48af3367e64a49ed3b6bdc1
github1tg <88201236+github1tg@users.noreply.github.com>
2021-12-10 09:50:55 +0530

    Update urls.py

--- bot/plugins/urls.py
+++ bot/plugins/urls.py
@@ -42,7 +42,7 @@ async def _(c, m):
     btns = Utilities.gen_ik_buttons()
 
     if duration >= 600:
-        btns.append([InlineKeyboardButton("Generate Sample Video!", "smpl")])
+        btns.append([InlineKeyboardButton("📸Generate Sample Video📸!", "smpl")])
 
     await snt.edit_text(
         text=f"Choose one of the options.\n\nTotal duration: `{datetime.timedelta(seconds=duration)}` (`{duration}s`)",
