aaa4a600b98617bc412c1667a7c3e1df3add39ac
github1tg <88201236+github1tg@users.noreply.github.com>
2021-12-10 19:18:21 +0530

    Update utils.py

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -312,21 +312,6 @@ class Utilities:
      
         return btns
  ________________________________________________________________________________________________________________________________________________________________
- reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton('🍬 Project channel', url='https://telegram.me/MHND_BOT_UPDATE_CHANNEL'),
-                    InlineKeyboardButton('🍭 Support group', url='https://telegram.me/MHND_BOT_UPDATE_GROUP')
-                ],
-                [
-                    InlineKeyboardButton('🌹 Source code 🌹', url='https://telegram.me/MHND_BOT_UPDATE_CHANNEL/145'),
-                    InlineKeyboardButton('👩‍💻 Master', url='https://telegram.me/MHND_KDR')
-                ]
-            ]
-        )
-    )
-
-________________________________________________________________________________________________________________________________________________________________
 
 buttons = [[
             InlineKeyboardButton('🇸 🇵 🇪 🇨 🇮 🇦 🇱 ', callback_data='style+special'),
