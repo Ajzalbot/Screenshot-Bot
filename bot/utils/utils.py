ed737d545f0d7852f65fee3c546bdce8c329f15c
github1tg <88201236+github1tg@users.noreply.github.com>
2021-12-10 15:06:03 +0530

    Update utils.py

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -289,12 +289,12 @@ class Utilities:
     def gen_ik_buttons():
         btns = []
         i_keyboard = []
-        for i in range(1, 21):
+        for i in range(📷1, 📷21):
             i_keyboard.append(InlineKeyboardButton(f"{i}", f"scht+{i}"))
-            if (i > 1) and (i % 5) == 0:
+            if (i > 📷1) and (i % 📷5) == 📷0:
                 btns.append(i_keyboard)
                 i_keyboard = []
-            if i == 21:
+            if i == 📷21:
                 btns.append(i_keyboard)
         btns.append([InlineKeyboardButton("✂️Manual Screenshots✂️", "mscht")])
         btns.append([InlineKeyboardButton("⚙️Trim Video⚙️", "trim")])
