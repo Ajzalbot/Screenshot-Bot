79879096a98f22a39dd5e8b328c7635c8dac3ab4
github1tg <88201236+github1tg@users.noreply.github.com>
2021-12-10 18:29:31 +0530

    Update utils.py

--- bot/utils/utils.py
+++ bot/utils/utils.py
@@ -296,7 +296,7 @@ class Utilities:
                 i_keyboard = []
             if i == 21:
                 btns.append(i_keyboard)
-        btns.appends(
+        btns.append(
             [
                  [
                  
